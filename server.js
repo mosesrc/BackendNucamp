ea1e07ca86e830000921e7af134691b963a4fefd
Ryan Moses <ryanchristophermoses@gmail.com>
2022-01-22 17:09:19 -0500

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express(); //Returns an express server application
+app.use(morgan("dev")); //This configures morgan to log using the developer version which prints more info to screen
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
-  console.log(req.header);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
